007b887a3a6e6f45305ef6031a70955c488ab271
Lucca Ribeiro <luccadmr@gmail.com>
2017-09-26 13:41:11 -0300

    Change in receiving JSON data
    Change in receiving JSON data

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -17,6 +17,95 @@ export class FeedPage {
 
   public nome_usuario:string = "Lucca Dumas Var";
 
+  public obj_feed = {
+    /*username: "Lucca Dumas",
+    date: "13 de Setembro de 2017",
+    description: "Primeiro App Teste Ionic.",
+    num_likes: 99,
+    num_comments: 8,
+    time_comment: "11h ago",*/
+
+    user: [
+      {
+        id: 123,
+        username: "Lucca Dumas",
+        photo: "photo.jpg",
+        address: [
+          {
+            street: "Avenida 77",
+            house_number: 77
+          },
+
+          {
+            street: "Avenida 777",
+            house_number: 777
+          }
+        ]
+      },
+      {
+        id: 456,
+        username: "Isabele Maria",
+        photo: "photo.jpg",
+        address: [
+          {
+            street: "Avenida 88",
+            house_number: 88
+          },
+
+          {
+            street: "Avenida 888",
+            house_number: 888
+          }
+        ]
+      },
+      {
+        id: 789,
+        username: "Francisco Caio",
+        photo: "photo.jpg",
+        address: [
+          {
+            street: "Avenida 99",
+            house_number: 99
+          },
+
+          {
+            street: "Avenida 999",
+            house_number: 999
+          }
+        ]
+      }
+    ],
+
+    post: [
+      {
+        date: "01 de abril de 1998",
+        description: "Primeiro App Teste Ionic1.",
+        img: "1.jpg",
+        num_likes: 77,
+        num_comments: 7,
+        time_comment: "7h atrás"
+      },
+
+      {
+        date: "20 de setembro de 2000",
+        description: "Primeiro App Teste Ionic2.",
+        img: "2.png",
+        num_likes: 88,
+        num_comments: 8,
+        time_comment: "8h atrás"
+      },
+
+      {
+        date: "01 de abril de 2000",
+        description: "Primeiro App Teste Ionic3.",
+        img: "3.jpg",
+        num_likes: 99,
+        num_comments: 9,
+        time_comment: "9h atrás"
+      },
+    ],
+  }
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
 
   }
@@ -25,9 +114,9 @@ export class FeedPage {
   public num2:number = 15;
   public res: number = this.num1 + this.num2;*/
 
-  public res:number;
+  public res: number;
 
-  public somaDoisNumeros(num1:number, num2:number):void{
+  public somaDoisNumeros(num1:number, num2:number): void {
     this.res = num1 + num2;
     //alert("Resultado: " + this.res);
   }
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 //import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 import { FeedPage } from '../feed/feed';
-import { ListaPage } from '../lista/lista';
+//import { ListaPage } from '../lista/lista';
 //import { Teste2Page } from '../teste2/teste2';
 //import { TestePage } from '../teste/teste';
 //import { IntroPage } from '../intro/intro';
@@ -18,7 +18,7 @@ export class TabsPage {
   //tab2Root = AboutPage;
   //tab3Root = ContactPage;
   tab4Root = FeedPage;
-  tab5Root = ListaPage;
+  //tab5Root = ListaPage;
   //tab7Root = Teste2Page;
   //tab6Root = TestePage;
   //tab4Root = IntroPage;
